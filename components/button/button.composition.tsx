96e8d4e999eb67e4de62c15106d95c8a77653572
sy <somarl@live.com>
2021-08-04 17:29:24 +0800

    feat(button): init

---
+++ components/button/button.composition.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './button'
+
+export const BasicButton = () => (
+    <Button label="hello from Button" />
+)
+
+export const Borderless = () => (
+    <Button borderless label="hello from Button" />
+)
+
+export const Disabled = () => (
+    <Button disabled label="hello from Button" />
+)
---
+++ components/button/button.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Flex from '@csszen/components.flexmini'
+import {moduledBem} from '@csszen/hooks.usebem'
+import styles from './button.module.scss'
+
+
+const useBem = moduledBem(styles)
+
+export interface IButtonProps {
+    type?: 'button' | 'submit' | 'reset'
+    name?: string
+    label?: string
+    loading?: boolean
+    disabled?: boolean
+    className?: string
+    labelClassName?: string
+    primary?: boolean
+    borderless?: boolean
+    onClick?: JSX.IntrinsicElements['button']['onClick']
+}
+
+
+export default function Button ({
+    type = 'button',
+    name,
+    label,
+    loading = false,
+    disabled = false,
+    className = '',
+    labelClassName = '',
+    onClick,
+    primary = false,
+    borderless = false,
+}: IButtonProps) {
+    return (
+        <button
+            type={type}
+            name={name}
+            className={useBem('button', '', {primary, borderless}) + ` ${className}`}
+            disabled={disabled || loading}
+            onClick={onClick}
+        >
+            <Flex>
+                <span className={styles.button__label + ' ' + labelClassName}>{label}</span>
+            </Flex>
+        </button>
+    )
+}
