deb4664bb60e73910584c4fe4acc4ed343596626
sy <somarl@live.com>
2021-08-30 18:51:37 +0800

    feat(copy): init

---
+++ components/copy/copy.composition.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Copy from './copy'
+
+export const BasicCopy = () => (
+    <section>
+        <h2>hello from Copy</h2>
+        <Copy content="hello from Copy" />
+    </section>
+)
