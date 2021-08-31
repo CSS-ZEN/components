03cbf5337843421dcf0db6d7a979b95aba3641d4
sy <somarl@live.com>
2021-08-31 11:18:22 +0800

    feat(octiconupload): init

---
+++ icons/octiconupload/octiconupload.composition.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Upload from './octiconupload'
+
+export const BasicOcticonupload = () => (
+    <section>
+        <h2>hello from Octiconupload</h2>
+        <Upload />
+    </section>
+)
